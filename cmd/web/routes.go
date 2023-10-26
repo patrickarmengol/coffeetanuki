b43d47b38071ab5e41fa43d237d85f65422e6855
Patrick Armengol <patrickarmengol@protonmail.com>
2023-10-26 20:20:22 +0000

    added embedding and routing for static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/patrickarmengol/coffeetanuki/ui"
 )
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+
+	// load fileserver on embedded static files
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	// overwrite default error response handlers
 	// router.NotFound = http.HandlerFunc(app.NotFoundResponse)
