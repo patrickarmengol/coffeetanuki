afd6940fe275ee47398b1157f9ab089c23a740c1
Patrick Armengol <patrickarmengol@protonmail.com>
2023-11-14 19:55:30 +0000

    changed user name to be case insensitive

--- migrations/000003_create_users_table.up.sql
+++ migrations/000003_create_users_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id bigserial PRIMARY KEY,
-    name text NOT NULL,
+    name citext NOT NULL,
     email citext UNIQUE NOT NULL,
     password_hash bytea NOT NULL,
     activated bool NOT NULL,
