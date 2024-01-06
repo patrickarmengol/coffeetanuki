31b60a2db58541158498e67e31369fd89e7ce1b0
Patrick Armengol <patrickarmengol@protonmail.com>
2024-01-06 22:43:54 +0000

    fixed typo in user permissions query

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -28,7 +28,7 @@ func (rep PermissionRepository) GetAllForUser(userID int64) (Permissions, error)
 	SELECT permissions.code
 	FROM permissions
 	INNER JOIN users_permissions ON users_permissions.permission_id = permissions.id
-	INNER JOIN users ON users_permissions.user_id = user.id
+	INNER JOIN users ON users_permissions.user_id = users.id
 	WHERE users.id = $1
 	`
 
